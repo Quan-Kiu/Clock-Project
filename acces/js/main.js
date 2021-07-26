df3c03833e1bce2cccabab3446f564be946dbd8b
quan-kiu <quankiugl@gmail.com>
2021-07-26 23:11:08 +0700

    update reposive

--- acces/js/main.js
+++ acces/js/main.js
@@ -1,41 +1,12 @@
-const hour = document.getElementById('hour'), minutes = document.getElementById('minutes'), seconds = document.getElementById('seconds');
+const hour = document.getElementById("hour"),
+  minutes = document.getElementById("minutes"),
+  seconds = document.getElementById("seconds"),
+  timestext = document.getElementById("times-text"),
+  datetext = document.getElementById("date-text"),
+  sun = document.getElementById("sun"),
+  moon = document.getElementById("moon");
 
-const timestext = document.getElementById('times-text'),datetext = document.getElementById('date-text');
-
-const clock = () =>{
-
-let date = new Date()
-
-hh = date.getHours() * 30,
-
-mm = date.getMinutes()*6,
-
-ss = date.getSeconds()*6;
-
-hour.style.transform = 'translateX(-50%) rotate('+((hh+(mm/12))+180)+'deg)';
-
-minutes.style.transform = 'translateX(-50%) rotate('+(mm+180)+'deg)';
-
-seconds.style.transform = 'translateX(-50%) rotate('+(ss+180)+'deg)';
-
-
-hh /= 30;
-mm /= 6;
-
-if(hh <10)
-{
-    hh = '0'+hh+'';
-}
-
-if(mm<10)
-{
-    mm = hh = '0'+mm+'';
-}
-
-
-
-timestext.innerHTML = ''+hh+':'+mm+'';
-
+let date = new Date();
 
 day = date.getDate();
 
@@ -43,9 +14,47 @@ month = date.getMonth();
 
 year = date.getFullYear();
 
-datetext.innerHTML = ''+day+'/'+month+'/'+year+'';
+hh = date.getHours();
 
+hh /= 30;
 
+datetext.innerHTML = "" + day + "/" + month + "/" + year + "";
+
+if (hh >= 5 && hh < 18) {
+  sun.style.display = "block";
+} else {
+  moon.style.display = "block";
 }
 
-setInterval(clock,1000)
+clock();
+
+function clock() {
+  date = new Date();
+
+  (hh = date.getHours() * 30),
+    (mm = date.getMinutes() * 6),
+    (ss = date.getSeconds() * 6);
+
+  hour.style.transform =
+    "translateX(-50%) rotate(" + (hh + mm / 12 + 180) + "deg)";
+
+  minutes.style.transform = "translateX(-50%) rotate(" + (mm + 180) + "deg)";
+
+  seconds.style.transform = "translateX(-50%) rotate(" + (ss + 180) + "deg)";
+
+  hh /= 30;
+
+  mm /= 6;
+
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+
+  if (hh < 10) {
+    hh = "0" + hh;
+  }
+
+  timestext.innerHTML = "" + hh + ":" + mm + "";
+
+  setTimeout(clock, 1000);
+}
